968c363d0286899f8ee16e641bd2917c7cf054df
VINAYSAI31 <vinaysai1133@gmail.com>
2024-12-22 06:07:15 +0530

    updated contactus

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,9 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 const Contact = () => {
+ 
+
+  
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
@@ -63,12 +66,77 @@ const Contact = () => {
               Have any questions? Reach out to us, and we will respond promptly!
             </h2>
             {isVisible && (
-          <p className="text-center text-white bg-gradient-to-r from-teal-400 to-blue-600 p-6 rounded-3xl text-xl font-bold mb-6 shadow-xl animate-bounce transition-all duration-500 hover:scale-105">
-          🚀 Hold tight! We're loading something awesome for you...{" "}
-          <span role="img" aria-label="sparkles">✨</span>
-        </p>
-        
+         <>
+         <p className="text-center text-white bg-gradient-to-r from-teal-400 to-blue-600 p-6 rounded-3xl text-xl font-bold mb-6 shadow-xl">
+  🚀 Hold tight! We're loading something awesome for you...{" "}
+  <span role="img" aria-label="sparkles">✨</span>
+</p>
+
+<div className="flex justify-center mb-10 mt-20">
+        <span className="loader"></span>
+      </div>
+<style jsx>{`
+        .loader {
+  color:rgb(64, 35, 208);
+  font-size: 45px;
+  text-indent: -9999em;
+  overflow: hidden;
+  width: 1em;
+  height: 1em;
+  border-radius: 50%;
+  position: relative;
+  transform: translateZ(0);
+  animation: mltShdSpin 1.7s infinite ease, round 1.7s infinite ease;
+}
+
+@keyframes mltShdSpin {
+  0% {
+    box-shadow: 0 -0.83em 0 -0.4em,
+    0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em,
+    0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+  }
+  5%,
+  95% {
+    box-shadow: 0 -0.83em 0 -0.4em, 
+    0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 
+    0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+  }
+  10%,
+  59% {
+    box-shadow: 0 -0.83em 0 -0.4em, 
+    -0.087em -0.825em 0 -0.42em, -0.173em -0.812em 0 -0.44em, 
+    -0.256em -0.789em 0 -0.46em, -0.297em -0.775em 0 -0.477em;
+  }
+  20% {
+    box-shadow: 0 -0.83em 0 -0.4em, -0.338em -0.758em 0 -0.42em,
+     -0.555em -0.617em 0 -0.44em, -0.671em -0.488em 0 -0.46em, 
+     -0.749em -0.34em 0 -0.477em;
+  }
+  38% {
+    box-shadow: 0 -0.83em 0 -0.4em, -0.377em -0.74em 0 -0.42em,
+     -0.645em -0.522em 0 -0.44em, -0.775em -0.297em 0 -0.46em, 
+     -0.82em -0.09em 0 -0.477em;
+  }
+  100% {
+    box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 
+    0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+  }
+}
+
+@keyframes round {
+  0% { transform: rotate(0deg) }
+  100% { transform: rotate(360deg) }
+}
+ 
+      `}</style>
+     
+         </>
+                
         )}
+ 
+
+
+     
 
             
             <div
